2335fa330cb0b06066a4a2e0274b5e6d91d1c000
Mostafa Safaie <mostafa.safaie@gmail.com>
2025-02-26 21:59:02 +0000

    new test

--- tests.py
+++ tests.py
@@ -2,11 +2,9 @@ import mempipe
 import numpy as np
 
 if __name__ == "__main__":
-    mp = mempipe.MemPipe()
-    mp.init(np.zeros((10, 10), dtype=np.float32))
+    mp = mempipe.MemPipe(np.zeros((10, 10), dtype=np.float64))
     p_in, p_out = mp.Pipe()
-    p_in.send(np.ones((10, 10), dtype=np.float32))
-    print(p_out.recv())
-    p_in.send(np.ones((10, 10), dtype=np.float32))
+    p_in.send(np.random.rand(10, 10))
+    p_in.poll()
     print(p_out.recv())
     mp.close()
